fe34919ac88a6fb35476c6a5f1e4b07afb79b4ab
Vlad Utyumov <utyv108@gmail.com>
2026-02-14 20:06:43 +0500

    Update content-loyder.js

--- docs/js/content-loyder.js
+++ docs/js/content-loyder.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/data/content.json')
+    fetch('/test-site/data/content.json')
         .then(response => response.json())
         .then(data => {
             const container = document.getElementById('dynamic-content');
